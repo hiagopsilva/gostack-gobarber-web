24bd41d7bfe6e60bbdc600c0c7076aa0c992a203
hiago <hiagohps360@gmail.com>
2021-03-04 23:28:00 -0300

    added validation yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
 
 import { Container, Content, Background } from './styles';
 import LogoImg from '../../assets/logo.svg';
@@ -10,21 +11,25 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: any) => {
-    // try {
-    //   const schema = Yup.object().shape({
-    //     name: Yup.string().required('Nome obrigatório'),
-    //     email: Yup.string()
-    //       .required('E-mail obrigatório')
-    //       .email('Digite um e-mail válido'),
-    //     password: Yup.string().min(6, 'No mínimo 6 dígitos'),
-    //   });
-    //   await schema.validate(data, {
-    //     abortEarly: false,
-    //   });
-    // } catch (err) {
-    //   console.log(err);
-    // }
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      formRef.current?.setErrors({
+        name: 'Nome Obrigatório',
+      });
+    }
   }, []);
   return (
     <Container>
@@ -33,7 +38,7 @@ const SignUp: React.FC = () => {
       <Content>
         <img src={LogoImg} alt="GoBarber" />
 
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
